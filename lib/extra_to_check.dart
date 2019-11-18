41b39f06f6cf12f0e748eb7aa6dd454fa4f8bb4f
Devansh Baldwa <devanshbaldwa@gmail.com>
2019-11-18 20:29:40 +0530

    Ready for Release

---
+++ lib/extra_to_check.dart
@@ -0,0 +1,35 @@
+              // if(_toCheckButtonPressed == 1)
+              //   Text("YOU CHOSE HEADS",style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600, fontStyle: FontStyle.italic),)
+
+              // else if(_toCheckButtonPressed == 2)
+              //   Text("YOU CHOSE TAILS",style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600, fontStyle: FontStyle.italic),)
+
+              // else
+              //   Text("",style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600, fontStyle: FontStyle.italic),),
+              
+              // SizedBox(height: 20,),
+
+              // if(_toCheckButtonPressed == 1 || _toCheckButtonPressed == 2)
+              //   Text("THE RESULT OF THE TOSS IS",style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600, fontStyle: FontStyle.italic),),
+
+
+              
+              // if(_toCheckButtonPressed != 1 && _toCheckButtonPressed != 2)
+              //   Image.asset("images/toss_image.png")
+              
+              // else if(next(1, 3) == 1)
+              //   Image.asset("images/Heads_Image.jpg")
+              
+              // else if(next(1, 3) == 2)
+              //   Image.asset("images/tails_image.jpeg")
+                
+              // else
+              //   Text(""),
+
+              // SizedBox(height: 20,),
+              // if(_toCheckButtonPressed == next(1, 3))
+              //   Text("YOU WIN")
+              // else if(_toCheckButtonPressed != next(1, 3) && _toCheckButtonPressed >= 1 && _toCheckButtonPressed <3)
+              //   Text("YOU LOSE")
+              // else
+              //   Text(""),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'about_me.dart';
+import 'dart:math';
 
 void main() => runApp(MyApp());
 
@@ -24,14 +25,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // List _buttonPressed = [0, 1, 2];
+  int _toCheckButtonPressed = 0;
+  final _random = new Random();
+  int next(int min, int max) => min + _random.nextInt(max - min);
+  int _answer = 5;
 
-  bool _buttonPressed;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
-        title: Text(widget.title,style: TextStyle(fontSize: 22.0),),
+        title: Text(
+          widget.title,
+          style: TextStyle(fontSize: 18.0),
+        ),
         actions: <Widget>[
           new IconButton(
               icon: Icon(Icons.info),
@@ -44,41 +52,163 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Container(
         color: Colors.cyan.shade100,
-        child: Column(
-            children: <Widget>[
-              SizedBox(height: 15.0,),
-              Text("Choose Heads or Tails",style: TextStyle(fontSize: 44.0, fontWeight: FontWeight.w600, fontStyle: FontStyle.italic),),
-              SizedBox(height: 8.0),
-              Text("Choose Wisely 😉", style: TextStyle(fontSize: 18.0),),
-              SizedBox(height: 50.0,),
-              ButtonBar(
-                alignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  RaisedButton(
-                    color: Colors.amberAccent,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text("HEADS",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 22.0),),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: 0.1,
+            ),
+            Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  "Choose Heads or Tails",
+                  style: TextStyle(
+                      fontSize: 34.0,
+                      fontWeight: FontWeight.w600,
+                      fontStyle: FontStyle.italic),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  "Choose Wisely 😉",
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                ButtonBar(
+                  alignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    RaisedButton(
+                      color: Colors.amberAccent,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "HEADS",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 22.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _toCheckButtonPressed = 1;
+                          _answer = next(1, 3);
+                          debugPrint("${_toCheckButtonPressed}");
+                        });
+                      },
                     ),
-                    onPressed: (){
-                      _buttonPressed = false;
-
-                    },
-                  ),
-                  RaisedButton(
-                    color: Colors.redAccent,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text("TAILS",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 22.0),),
+                    RaisedButton(
+                      color: Colors.redAccent,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "TAILS",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 22.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _toCheckButtonPressed = 2;
+                          _answer = next(1, 3);
+                          debugPrint("${_toCheckButtonPressed}");
+                        });
+                      },
                     ),
-                    onPressed: (){
-                      _buttonPressed = true;
-                    },
+                  ],
+                ),
+                SizedBox(height: 10.0),
+                if (_toCheckButtonPressed == 1)
+                  Text(
+                    "YOU CHOSE HEADS...",
+                    style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w600,
+                        fontStyle: FontStyle.italic),
+                  )
+                else if (_toCheckButtonPressed == 2)
+                  Text(
+                    "YOU CHOSE TAILS...",
+                    style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w600,
+                        fontStyle: FontStyle.italic),
+                  )
+                else
+                  Text(""),
+                Divider(
+                  thickness: 4.0,
+                  color: Colors.black87,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                if (_toCheckButtonPressed == 1 || _toCheckButtonPressed == 2)
+                  if (_answer == 1)
+                    Text(
+                      "THE RESULT OF THE TOSS IS HEADS",
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic),
+                    )
+                  else if (_answer == 2)
+                    Text(
+                      "THE RESULT OF THE TOSS IS TAILS",
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic),
+                    )
+                  else
+                    Text(""),
+                SizedBox(
+                  height: 15.0,
+                ),
+                if (_answer == 1)
+                  Image.asset("images/heads_image.jpeg")
+                else if (_answer == 2)
+                  Image.asset("images/tails_image.jpeg")
+                else if (_answer == 5)
+                  Image.asset("images/toss_image.png")
+                else
+                  Text(""),
+                SizedBox(
+                  height: 15.0,
+                ),
+                if (_answer == _toCheckButtonPressed)
+                  Text(
+                    "YOU WIN 😎",
+                    style: TextStyle(
+                        fontSize: 54.0,
+                        fontWeight: FontWeight.w900,
+                        fontStyle: FontStyle.italic),
+                  )
+                else if (_answer != _toCheckButtonPressed &&
+                    _answer > 0 &&
+                    _answer < 3 &&
+                    _toCheckButtonPressed > 0 &&
+                    _toCheckButtonPressed < 3)
+                  Text(
+                    "YOU LOSE 😓",
+                    style: TextStyle(
+                        fontSize: 54.0,
+                        fontWeight: FontWeight.w900,
+                        fontStyle: FontStyle.italic),
+                  )
+                else
+                  Text(
+                    "",
+                    style: TextStyle(
+                        fontSize: 54.0,
+                        fontWeight: FontWeight.w900,
+                        fontStyle: FontStyle.italic),
                   ),
-                ],
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
